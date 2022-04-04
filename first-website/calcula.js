fa3ab3c6ebb06438d97a6ad974c7b53dcc561d9c
4N70N123 <97764658+4N70N123@users.noreply.github.com>
2022-04-04 12:34:33 -0500

    Add files via upload

---
+++ first-website/calcula.js
@@ -0,0 +1,28 @@
+function calc(){
+    var numero1 = document.getElementByName("numero-1");
+    var numero2 = document.getElementByName("numero-2");
+    var calcular = document.getElementByName("boton");
+    var resultado = document.getElementByName("Resultado");
+    var operacion = document.getElementByName("operacion");
+    var res;
+    var a = numero1.textContent;
+    var b = numero2.textContent;
+
+    calcular.onclick = function(e) {
+        switch(operacion){
+            case "Sumar":
+                res = parseFloat(a) + parseFloat(b);
+            break;
+            case "Restar":
+                res = parseFloat(a) - parseFloat(b);
+            break;
+            case "Multiplicar":
+                res = parseFloat(a) * parseFloat(b);
+            break;
+            case "Dividir":
+                res = parseFloat(a) / parseFloat(b);
+            break;
+        }
+        resultado.textContent = res;
+    }
+}
